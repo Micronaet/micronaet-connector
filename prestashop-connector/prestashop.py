405a072f3aa9f75f65ec9c6b15bc9ca1a4335680
nicothebrush <nicola.riolini@gmail.com>
2017-03-20 18:46:53 +0100

    publish using album path

--- prestashop-connector/prestashop.py
+++ prestashop-connector/prestashop.py
@@ -47,6 +47,7 @@ class ProductProductWebServer(orm.Model):
     def publish_now_prestashop(self, cr, uid, ids, context=None):
         ''' Publish procedure for prestashop element
         '''
+        langs = ['it_IT', 'en_US']
         # No category publish (get from Prestashop not created here!)
         _logger.info('Start publish prestashop product')        
         connector_pool = self.pool.get('connector.server')
@@ -63,11 +64,12 @@ class ProductProductWebServer(orm.Model):
         sock = connector_pool.get_prestashop_connector(
             cr, uid, [connector.id], context=context)
 
-        album_id = first_proxy.connector_id.album_id.id
+        album = first_proxy.connector_id.album_id
+        album_id = album.id
         #context['album_id'] = first_proxy.connector_id.album_id.id
         
         rpc_default_code = {}
-        path_image_in = '/home/thebrush/images/Varie Roberta/FOTO OK/SCONTORNATE/' # TODO from album
+        path_image_in = os.path.expanduser(album.path)
         for item in self.browse(cr, uid, ids, context=db_context):
             # Readability:
             product = item.product_id
@@ -84,24 +86,26 @@ class ProductProductWebServer(orm.Model):
             
             # Rsync data image file: XXX choose if needed
             rsync_command = \
-                'rsync --chown %s --chmod %s -avh -e \'ssh -p %s\' \'%s%s\' %s@%s:%s' % (
+                'rsync --chown %s --chmod %s -avh -e \'ssh -p %s\' \'%s\' %s@%s:%s' % (
                     connector.rsync_chown,
                     connector.rsync_chmod,
                     connector.rsync_port,
-                    path_image_in, 
-                    image_in, 
+                    os.path.join(path_image_in, image_in), 
                     connector.rsync_user, 
                     connector.host,
                     connector.rsync_path,
                     )
             os.system(rsync_command)
+            _logger.info('Launched: %s' % rsync_command)
 
             price = item.force_price or product.lst_price # XXX correct?
             #image = product.product_image_context # from album_id
             #public_categ_ids = [self.odoo_web_db.get(
             #    c.id) for c in product.public_categ_ids]
             
-            # Standard data:    
+            # -----------------------------------------------------------------
+            # Standard record data:    
+            # -----------------------------------------------------------------
             record = {
                 'reference': default_code or '', 
                 'ean13': product.ean13 or '',
@@ -115,24 +119,33 @@ class ProductProductWebServer(orm.Model):
                 #'vat_price': price * 1.22,      
                 #'public_categ_ids': [(6, 0, public_categ_ids)],
                 } 
-                
-            record_lang = {
-                'it_IT': {
-                    'name': item.force_name or product.name or '', # title
-                    'meta_title': item.force_name or product.name or '', # XXX
+            
+            # -----------------------------------------------------------------
+            # Lang record: 
+            # -----------------------------------------------------------------
+            record_lang = {}
+            context_lang = context.copy()            
+            for lang in langs:
+                context_lang['lang'] = lang
+                item_lang = self.browse(
+                    cr, uid, item.id, context=context_lang)
+                record_lang[lang] = {
+                    # Title:
+                    'name': item_lang.force_name or \
+                        item_lang.product_id.name or '',
+                    'meta_title':
+                        item_lang.force_name or \
+                            item_lang.product_id.name or '',
                     'meta_description': 
-                        item.force_description or product.large_description or '',
-                #'fabric': product.fabric,
-                #'type_of_material': product.type_of_material,
-                    }, 
-                # TODO lang:                        
-                'en_US': {
-                    'name': 'ENCon immagine',
-                    'meta_title': 'ENTitolo della sedia di prova con misure',
-                    'meta_description': 'ENDescrizione della sedia di prova',
-                    },
-                }
-                
+                        item_lang.force_description or \
+                            item_lang.product_id.large_description or '',
+                    #'fabric': product.fabric,
+                    #'type_of_material': product.type_of_material,
+                    }
+
+            # -----------------------------------------------------------------
+            # Categoryy record
+            # -----------------------------------------------------------------
             category = {
                 # id_product
                 'id_category': product.public_categ_ids[0].website_id if \
@@ -147,30 +160,6 @@ class ProductProductWebServer(orm.Model):
                 )
 
         _logger.info('Update other product lang:')
-
-        '''for lang in self._langs:  
-            if lang == self._lang_db:
-                continue # no default lang, that create object!
-                
-            db_context['lang'] = lang
-            rpc.context = db_context
-            for item in self.browse(cr, uid, ids, context=db_context):
-                product = item.product_id
-                default_code = product.default_code
-                # Get product ID from database:
-                product_ids = rpc_default_code.get(default_code, [])
-                
-                # Update language:
-                if not product_ids:
-                    _logger.warning('No lang %s for %s' % (lang, default_code))
-                    continue
-                rpc_product.write(product_ids, {
-                    'name': item.force_name or product.name,
-                    'description_sale': 
-                        item.force_description or product.large_description,
-                    'fabric': product.fabric,
-                    'type_of_material': product.type_of_material,
-                    })'''
         return True
         
     # Override action for test if prestashop product
